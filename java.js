b0c7da3c87ab3121b84215e04b57c87e3faad71c
Jding6 <29john.dingel@boiseschools.net>
2026-02-10 09:18:53 -0700

    Update java.js

--- java.js
+++ java.js
@@ -8,3 +8,15 @@ var colorAnswer=
 askQuestion("What is your favorite music artist?");
 
 console.log(alert("Nice Mine too!"));
+var itemNum= prompt("Did you buyy 1 or 2 items?");
+function check(v){
+  if (v==1){
+  }else{
+    alert("You can have a free one!")'
+  }
+}
+check(itemNum);
+itemNum = prompt("Did you buy 1 or 2 items?");
+check(itemNum)
+itemNum = prompt("Did you buy 1 or 2 items?");
+check(itemNum);
