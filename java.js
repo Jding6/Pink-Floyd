5fb9e84f8baf7f2900b6c202be54e77f1fb00113
Jding6 <29john.dingel@boiseschools.net>
2026-02-10 08:50:09 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,10 @@
 prompt("Have you ever listened to Pink Floyd");
+function askQuestion(question) {
+var response = prompt(question);
+return response;
+}
+
+var colorAnswer= 
+askQuestion("What is your favorite music artist?");
+
+console.log(alert("Nice Mine too!"));
